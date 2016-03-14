a8501b4ad648dd3430d58ab7ea19ee8537fe10ce
mariosky <mariosky@gmail.com>
2016-03-14 14:30:49 -0700

    evospace get sample

--- routes/evospace.js
+++ routes/evospace.js
@@ -7,18 +7,47 @@ var evospace = require('../lib/evospace');
 var router = express.Router();
 
 
-var population =  new evospace.Population("pop");
+
 
 
 /* GET home page. */
-router.get('/card', function(req, res, next) {
+router.get('/:space/cardinality', function(req, res, next) {
+
+    var population =  new evospace.Population(req.space);
 
     population.card( function(err, result)
     {
+
         res.send( { 'cardinality': result } );
     });
 
 });
+
+router.put('/:space/initialize', function(req, res, next) {
+
+    var population =  new evospace.Population(req.space);
+
+    population.initialize( function(err, result)
+    {
+
+        res.send( { 'result': result } );
+    });
+
+});
+
+
+router.get('/:space/get_sample/:size', function(req, res, next) {
+
+    var population =  new evospace.Population(req.space);
+
+    population.get_sample( req.size ,function(err, result)
+    {
+
+        res.send( { 'result': result } );
+    });
+
+});
+
 
 
 module.exports = router;
