de44f60737f0ebbf0e3882000ea73d127b21fd8a
Mario Garcia Valdez <mariosky@gmail.com>
2016-03-26 23:59:50 -0700

    evospace: Respawn n

--- public/javascripts/evospace-dashboard.js
+++ public/javascripts/evospace-dashboard.js
@@ -2,23 +2,7 @@
  * Created by mario on 3/26/16.
  */
 $(document).ready(function () {
-
-    $.get('/evospace/pop')
-        .done(function (data) {
-            console.log(data);
-
-            $("#population").html(data.population.join(", ")) ;
-
-        });
-
-    $.get('/evospace/pop/sample_queue')
-        .done(function (data) {
-            console.log(data);
-
-            $("#sample-queue").html(data.result.join(", ")) ;
-
-        });
-
+    refresh_state();
     $("#respawn").click(function () {
         $.post('/evospace/pop/respawn/',{n:1})
             .done(function (data) {
@@ -29,9 +13,27 @@ $(document).ready(function () {
         });
 
 
-
-
-
-
-
 });
+
+var refresh_state = function(){
+$.get('/evospace/pop')
+    .done(function (data) {
+        console.log(data);
+
+        $("#population").html(data.population.join(", ")) ;
+
+    });
+
+$.get('/evospace/pop/sample_queue')
+    .done(function (data) {
+        console.log(data);
+
+        $("#sample-queue").html(data.result.join(", ")) ;
+
+    });
+}
+
+
+var interval = 1000; // where X is your every X minutes
+
+setInterval(refresh_state, interval);
