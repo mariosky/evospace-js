266f7887b336a8200a98d7a1de23a176d7604712
Luis Rodriguez <isc.luisrodriguez@gmail.com>
2016-04-21 17:44:08 -0700

    EvoDraw Population Dashboard Test

--- routes/evospaceTest.js
+++ routes/evospaceTest.js
@@ -6,32 +6,43 @@ var evospace = require('../lib/evospace');
 var router = express.Router();
 
 /* GET home page. */
+router.route('/')
+    .get(function(req, res) {
+        res.render('evospace-dashboard-test',  {title: 'Populations'});
+    })
+    .post(function(req, res) {
+        var namePopulation = req.body.name;
+        var numberInd = req.body.numberInd;
+        var pop =  new evospace.Population(namePopulation);
 
-router.get('/', function(req, res, next) {
-    res.render('evospace-dashboard-test',  {title: 'Populations'});
-
-});
-
-router.post('/addPopulation', function(req, res, next) {
-    var namePopulation = req.body.name;
-    var numberInd = req.body.numberInd;
-    var pop =  new evospace.Population(namePopulation);
-
-    pop.initialize(function(err,results)
-        {
-            for (var x= 0; x<numberInd; x++)
+        pop.initialize(function(err,results)
             {
-                var i = new evospace.Individual( {id:x, name:"Mario", chromosome:[1,2,3,1,1,2,2,2],"fitness":{"s":1} } )
-                console.log(JSON.stringify(i));
-                i.put(namePopulation);
+                for (var x= 0; x<numberInd; x++)
+                {
+                    var i = new evospace.Individual( {id:x, name:"Mario", chromosome:[1,2,3,1,1,2,2,2],"fitness":{"s":1} } )
+                    console.log(JSON.stringify(i));
+                    i.put(namePopulation);
+                }
             }
-        }
-    );
+        );
 
-    pop.save_name_population(namePopulation);
+        var populations = new evospace.namePopulations();
+        populations.save_name_population(namePopulation);
 
-    res.redirect('/evospaceTest');
-});
+        res.redirect('/evospaceTest');
+    })
+    .put(function(req,res){
+        //res.send("PUT" + req.params.space);
+    })
+    .delete(function(req, res) {
+
+        var namePopulation = req.body.namePopulation;
+        var populations = new evospace.namePopulations();
+
+        populations.delete_name_population(namePopulation);
+        res.redirect('/evospaceTest');
+
+    });
 
 // Read All names of populations
 router.get('/namePopulations', function(req, res, next) {
@@ -42,12 +53,6 @@ router.get('/namePopulations', function(req, res, next) {
     populations.read_names_populations(name ,function(err, result) {
         res.send( result );
     });
-});
-
-router.delete('/:space', function (req, res) {
-
-    console.log(req.params.space);
-    res.send( req.params.space);
 });
 
 
