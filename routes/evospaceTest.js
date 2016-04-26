9ba07aff2cd03e092589a761b3c2a2e3ee00b395
Luis Rodriguez <isc.luisrodriguez@gmail.com>
2016-04-26 14:41:02 -0700

    EvoDraw Population Dashboard Test

--- routes/evospaceTest.js
+++ routes/evospaceTest.js
@@ -44,6 +44,41 @@ router.route('/')
 
     });
 
+router.route('/:space/dashboard')
+    .get(function(req, res) {
+        res.render('evospace-population-dashboard',  {title: req.params.space, namePop: req.params.space});
+        //res.send("Dashboard " + req.params.space);
+    })
+    .post(function(req, res) {
+        var size = req.body.sizeSample;
+
+        //res.send( { 'result': size } );
+        var population =  new evospace.Population(req.params.space);
+        population.get_sample( size ,function(err, result)
+        {
+            //res.send( { 'result': result } );
+        });
+        res.redirect('/evospaceTest/'+req.params.space+'/dashboard');
+        //res.redirect('evospace-population-dashboard',  {title: req.params.space});
+
+    })
+    .put(function(req,res){
+        //res.send("PUT" + req.params.space);
+    })
+    .delete(function(req, res) {
+        var namePopulation = req.body.namePopulation;
+        var idSample = req.body.idSample;
+
+        //res.send( { 'result': idSample } );
+        var pop =  new evospace.Population(namePopulation);
+        //pop.respawn_sample(idSample);
+        pop.respawn_sample(idSample,function(err, result)
+        {
+            //res.send( { 'result': result } );
+        });
+        res.redirect('/evospaceTest/'+ namePopulation + '/dashboard');
+    });
+
 // Read All names of populations
 router.get('/namePopulations', function(req, res, next) {
     var name = "namePopulations";
@@ -53,6 +88,24 @@ router.get('/namePopulations', function(req, res, next) {
     populations.read_names_populations(name ,function(err, result) {
         res.send( result );
     });
+});
+
+// Read All members of a sample
+router.get('/sample/:sample', function(req, res, next) {
+
+    var name = req.params.sample;
+    var populations = new evospace.namePopulations();
+
+
+    populations.read_sample(name ,function(err, result) {
+        res.send( result );
+    });
+});
+
+// Read All members of a sample
+router.get('/sample/:sample/dashboard', function(req, res, next) {
+
+    res.render('evospace-sample-dashboard',  {title: req.params.sample, nameSample: req.params.sample});
 });
 
 
